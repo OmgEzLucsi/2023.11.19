e16470c6d53f46fdcdfec72de992594b2425b13f
OmgEzLucsi <bolonyip@gmail.com>
2023-11-26 10:29:51 +0100

    init kommi

---
+++ Ts script feladat/script.ts
@@ -0,0 +1,59 @@
+/*
+TYPESCRIPT
+
+- Keszits alprogramot, ami ...
+    - Eldonti, hogz a parameterul kapott szamok tombben van-e negativ szam
+    -Egy parameterul kapott sugarbol megallapitja a kor keruletet es teruletet
+        -Tuple-t alkalmazz
+        -Kerulet: 2 * sugar * PI
+        -Terulet: sugar * sugar *PI
+
+-Keszits interface-t Auto neven
+    -Gyarto (szoveg)
+    -Tipus (szoveg)
+    -Hengerurtartalom (szam)
+    -BenzinesE (logikai)
+*/
+
+interface Auto{
+    gyarto:string;
+    tipus:string;
+    hengerurtartalom:number;
+    benzinesE:boolean;
+}
+
+
+
+function VanENegativ(szamok:number[]):boolean{
+    var i:number =0;
+    while(i <szamok.length && !(szamok[i] <0)){
+        i++;
+    }
+    return i <szamok.length;
+}
+
+function KorKerTer(r:number):[number, number]{
+    var ker:number = 2 * r * Math.PI;
+    var ter:number = r * r * Math.PI;
+
+    return [ker, ter];
+
+}
+
+console.log(VanENegativ([5, 12, 0, 23, -1, 54]));
+console.log(VanENegativ([5, 12, 0, 23, 54]));
+console.log(KorKerTer(3));
+
+/*
+-Keszits publikus GitHub repositorit a sajat felhasznaloi fiokodban
+-Indits Git BASH-t, es a jelenlegi Ts projektedben inicalizalj git-et
+-A lokalis repot-t kosd ossze az online (github-os) repoddal
+-Toltsd fel a jelenlegi allapotot a github-ra
+
+TS!!
+-Keszits alprogramot, ami kivalasztja egz auto tombbol a legkisebb hengerurtartalmu autot
+-Keszits alprogramot, ami megadja a parameterul kapott auto tombbol a benzinesek darabszamat
+-A valtoztatasokat tolsd fel a github repodba*/
+
+
+
